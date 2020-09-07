b0d3412d68fdc790ebaed658560a7fd8eca1685f
Maik Riestock <maik.riestock@ovgu.de>
2020-09-07 11:39:22 +0200

    Adds dependencies and members

--- include/example.h
+++ include/example.h
@@ -1,6 +1,10 @@
 // ros
 #include <ros/ros.h>
 #include <std_msgs/Header.h>
+#include <sensor_msgs/CameraInfo.h>
+#include <sensor_msgs/image_encodings.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
 
 namespace example_namespace
 {
@@ -13,13 +17,19 @@ namespace example_namespace
 
     ros::NodeHandle m_nh;
     ros::NodeHandle m_priv_nh;
-    ros::Subscriber m_sub_human_list;
-    ros::Publisher m_pub_human_list;
+    ros::Subscriber m_sub_camera_info;
+    image_transport::ImageTransport m_it;
+    image_transport::Subscriber m_it_sub_image;
+    image_transport::Publisher m_it_pub_image;
 
-    std::string m_topic_sub;
-    std::string m_topic_pub;
+    std::string m_topic_sub_image;
+    std::string m_topic_sub_camera_info;
+    std::string m_topic_pub_image;
 
     bool m_running;
+
+    void subscriberCallbackImage(const sensor_msgs::ImageConstPtr &msg_image);
+    void subscriberCallbackCameraInfo(const sensor_msgs::CameraInfoConstPtr &msg_camera_info);
 
   public:
     ExampleClassNode(const ros::NodeHandle &nh = ros::NodeHandle(), const ros::NodeHandle &priv_nh = ros::NodeHandle("~"));
